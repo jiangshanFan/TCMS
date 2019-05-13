77f610328466b5a39f07aaf537600eca1ab462fc
jiangshan.fan <745989449@qq.com>
2019-05-13 14:37:24 +0800

    index.scss background: error?

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
   <div id="app">
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2" class="el-menu-vertical-demo"
+          background-color="#082B50" text-color="#fff" active-text-color="#2C8CF0">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-container>
-        <el-header>Header</el-header>
-        <el-main>
+        <el-header class="header">Header</el-header>
+        <el-main class="main">
           <p>Main</p>
           <router-view />
         </el-main>
-        <el-footer>Footer</el-footer>
+        <el-footer class="footer">Footer</el-footer>
       </el-container>
     </el-container>
   </div>
@@ -25,13 +48,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  @import "./style/index.scss";
+  @import "./style/common.scss";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
 }
 </style>
